9ccee418fd4bd57ed369c1a17f3843654added6d
Kareem Farid <kareefardi@users.noreply.github.com>
2025-10-24 21:05:57 +0000

    fix script

--- patch_kernel.sh
+++ patch_kernel.sh
@@ -37,9 +37,8 @@
 
 
 # Copy SUSFS patches
-cp ./susfs4ksu/kernel_patches/50_add_susfs_in_gki-android13-5.10.patch 
+cp ./susfs4ksu/kernel_patches/50_add_susfs_in_gki-android13-5.10.patch .
 cp ./susfs4ksu/kernel_patches/fs/* ./fs/
 cp ./susfs4ksu/kernel_patches/include/linux/* ./include/linux/
 
-cd common
 patch -p1 --fuzz=3 < 50_add_susfs_in_gki-android13-5.10.patch || true
